15bec5296c0cf80550b70e36ec27a1510a17a5cb
nurita <nuritaeka2@gmail.com>
2023-10-07 20:14:14 +0700

    Kriteria 5: API dapat menampilkan detail buku (500: Internal Server error 'id' of 'request.param' as it is undefined)

--- src/handler.js
+++ src/handler.js
@@ -53,4 +53,22 @@ const getAllBookHandler = () => ({
         books
     }
 });
-module.exports = { addBookHandler, getAllBookHandler };
+const getBookByIdHanlder = (request, h) => {
+    const { id } = request.param;
+    const book = book.filter((b) => b.id === id)[0];
+    if(book !== undefined){
+       return {
+        status: 'success',
+        data: {
+            book,
+        },
+       };
+    }
+    const response = h.response({
+        status: 'fail',
+        message: 'Buku tidak ditemukan',
+    });
+    response.code(404);
+    return response;
+};
+module.exports = { addBookHandler, getAllBookHandler, getBookByIdHanlder };
